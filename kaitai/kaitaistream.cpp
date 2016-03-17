1900d916b8219f3a584e5e14dfb9fc3f818de986
Mikhail Yakshin <greycat@altlinux.org>
2016-03-17 15:42:41 +0300

    Alternative innard member syntax

--- kaitai/kaitaistream.cpp
+++ kaitai/kaitaistream.cpp
@@ -1,33 +1,33 @@
 #include <kaitai/kaitaistream.h>
 
 KaitaiStream::KaitaiStream(std::istream* io) {
-    _io = io;
+    io_ = io;
 }
 
 void KaitaiStream::close() {
-    //  _io->close();
+    //  io_->close();
 }
 
 uint8_t KaitaiStream::read_u1() {
     char t;
-    _io->get(t);
+    io_->get(t);
     return t;
 }
 
 uint16_t KaitaiStream::read_u2le() {
     uint16_t t;
-    _io->read(reinterpret_cast<char *>(&t), 2);
+    io_->read(reinterpret_cast<char *>(&t), 2);
     return t;
 }
 
 uint32_t KaitaiStream::read_u4le() {
     uint32_t t;
-    _io->read(reinterpret_cast<char *>(&t), 4);
+    io_->read(reinterpret_cast<char *>(&t), 4);
     return t;
 }
 
 uint64_t KaitaiStream::read_u8le() {
     uint64_t t;
-    _io->read(reinterpret_cast<char *>(&t), 8);
+    io_->read(reinterpret_cast<char *>(&t), 8);
     return t;
 }
--- kaitai/kaitaistream.h
+++ kaitai/kaitaistream.h
@@ -11,12 +11,25 @@ public:
     void close();
 
     uint8_t read_u1();
+
     uint16_t read_u2le();
     uint32_t read_u4le();
     uint64_t read_u8le();
 
+    uint16_t read_u2be();
+    uint32_t read_u4be();
+    uint64_t read_u8be();
+
+    sint16_t read_s2le();
+    sint32_t read_s4le();
+    sint64_t read_s8le();
+
+    sint16_t read_s2be();
+    sint32_t read_s4be();
+    sint64_t read_s8be();
+
 private:
-    std::istream* _io;
+    std::istream* io_;
 };
 
 #endif
